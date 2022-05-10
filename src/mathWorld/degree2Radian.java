a6bbe0d690b914055b699c460c1cb3d8fa66a4f5
Sten Uuesoo <sten.uuesoo@gmail.com>
2022-05-10 15:31:12 +0300

    Cleaned up unneeded components.

--- src/mathWorld/degree2Radian.java
+++ src/mathWorld/degree2Radian.java
@@ -7,25 +7,17 @@ public class degree2Radian {
             Java program to convert degree to radian. The radian is the standard unit of angular measure, 'used in many areas of mathematics.
             An angle's measurement in radians is numerically equal to the length of a corresponding arc of a unit circle.
             """;
-    static String getDesc() {
-        return desc;
-    }
     static double initialDegrees = 15;
-
-    static double getInitialDegrees() {
-        return initialDegrees;
-    }
     static double calculate2Radian() {
-        double initialDegrees = getInitialDegrees();
         return initialDegrees * (PI/180);
     }
     static String concatenatedRadians() {
         return String.format("%.5f", calculate2Radian());
     }
+    static String degree2RadianSolution = (desc + "\nTo convert: " + initialDegrees + " degrees" +
+            " | Converted to: " + concatenatedRadians() + " radians.");
 
     public static String getDegree2Radian() {
         return degree2RadianSolution;
     }
-    static String degree2RadianSolution = (getDesc() + "\nTo convert: " + getInitialDegrees() + " degrees" +
-            " | Converted to: " + concatenatedRadians() + " radians.");
 }
