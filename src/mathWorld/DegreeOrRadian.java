9c1a8e772966ddfe364fb1b6a820423144c915dc
Sten Uuesoo <sten.uuesoo@gmail.com>
2022-05-11 16:06:22 +0300

    Changed code structure so it's easier to follow.

--- src/mathWorld/DegreeOrRadian.java
+++ src/mathWorld/DegreeOrRadian.java
@@ -7,11 +7,21 @@ import static java.lang.Math.PI;
 public class DegreeOrRadian {
     static String desc = "Degree 2 Radian or Radian 2 Degree converter.";
     static String longDesc = """
-            Java program to convert Degrees to Radians or Radians to Degrees. The Radian is the standard unit of angular measure, used in many areas of mathematics.
+            Java program 2 convert Degrees to Radians or Radians to Degrees. The Radian is the standard unit of angular measure, used in many areas of mathematics.
             An angle's measurement in radians is numerically equal to the length of a corresponding arc of a unit circle.
             """;
     public static String getDesc() {
         return desc;
+    }
+
+    static String transformRadianToDegree(int mathMethod) {
+        return concatenateResult(setInitialDegrees(mathMethod) * (180/PI));
+    }
+    static String transformDegreeToRadian(int mathMethod) {
+        return concatenateResult(setInitialDegrees(mathMethod) * (PI/180));
+    }
+    static String concatenateResult(double transformedResult) {
+        return String.format("%.3f", transformedResult);
     }
     static double setInitialDegrees(int mathMethod) {
         Scanner scanner = new Scanner(System.in);
@@ -21,15 +31,6 @@ public class DegreeOrRadian {
         }
         System.out.println("Converting Radians into Degrees. Input Radians from 0 to 6.2835");
         return scanner.nextDouble();
-    }
-    static String transformRadianToDegree(int mathMethod) {
-        return concatenateResult(setInitialDegrees(mathMethod) * (180/PI));
-    }
-    static String transformDegreeToRadian(int mathMethod) {
-        return concatenateResult(setInitialDegrees(mathMethod) * (PI/180));
-    }
-    static String concatenateResult(double transformedResult) {
-        return String.format("%.3f", transformedResult);
     }
     public static void transformDegreeOrRadian() {
         Scanner scanner = new Scanner(System.in);
