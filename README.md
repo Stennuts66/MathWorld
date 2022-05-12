5917fd453971a6d7b30a17ade804beade63326cf
Sten Uuesoo <sten.uuesoo@gmail.com>
2022-05-12 11:39:02 +0300

    catch scanner string exception removed redundant methods

--- README.md
+++ README.md
@@ -4,22 +4,21 @@ Welcome, 2 Math World! This is one of my hobbies of learning Java and Math in tw
 The goal is in time use more sophisticated method and algorithms for the whole system.
 This is the console output:
 
+---------------------------
 ---Welcome to Math World---
-
+---------------------------
 0 | Degree 2 Radian or Radian 2 Degree converter.
-
-Choose your math tool:
+-----
+Would you like to solve a Math problem? Write a number from 0 to 10. 10 to exit.
 0
-Java program to convert Degrees to Radians or Radians to Degrees. The Radian is the standard unit of angular measure, used in many areas of mathematics.
+Java program 2 convert Degrees to Radians or Radians to Degrees. The Radian is the standard unit of angular measure, used in many areas of mathematics.
 An angle's measurement in radians is numerically equal to the length of a corresponding arc of a unit circle.
 
 Would you like to convert Degrees to Radians or Radians to Degrees? Choose 0 or 1:
 1
-
-Converting Radians into Degrees. Input Radians from 0 to 6.283
-
-6.283
-359.989
+Converting Radians into Degrees. Input Radians from 0 to 6.2835
+1
+57.296
 
 
 ---
--- src/MathLoader.java
+++ src/MathLoader.java
@@ -4,26 +4,37 @@ import java.util.Scanner;
 
 public class MathLoader {
     static ArrayList<String> mathsDescriptions = new ArrayList<>();
-    public static void getMath() {
-        addMathsToList();
-        printMathChoices();
-        int mathMethodNumber = chooseMath();
-        doMath(mathMethodNumber);
-    }
     static void addMathsToList() {
         mathsDescriptions.add(DegreeOrRadian.getDesc());
     }
-    public static void printMathChoices() {
+    public static void printChoices() {
         mathsDescriptions.forEach((mathDesc) -> System.out.println(mathDesc.indexOf(mathDesc) + " | " + mathDesc));
     }
-    public static int chooseMath() {
-        System.out.println("\nChoose your math tool:");
-        Scanner scanner = new Scanner(System.in);
+    public static int chooseMathMethod() {
+        Scanner scanner;
+        do {
+            System.out.println("-----\nWould you like to solve a Math problem? Write a number from 0 to 10. 10 to exit.");
+            scanner = new Scanner(System.in);
+        } while (!scanner.hasNextInt());
         return scanner.nextInt();
     }
-    public static void doMath(int mathMethodNumber) {
+    public static void selectMath(int mathMethodNumber){
         if (mathMethodNumber == 0) {
             DegreeOrRadian.transformDegreeOrRadian();
         }
+    }
+    public static void doMath() {
+        addMathsToList();
+        int mathMethodNumber;
+        do {
+            System.out.println("---------------------------\n---Welcome to Math World---\n---------------------------");
+            printChoices();
+            mathMethodNumber = chooseMathMethod();
+            if (mathMethodNumber == 10) {
+                break;
+            } else {
+                selectMath(mathMethodNumber);
+            }
+        } while (10 != mathMethodNumber);
     }
 }
